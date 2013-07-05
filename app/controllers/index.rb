55fe9bb9d841d83153ec6491639bb18cd7f98bfe
dbc-apprentice <dbc@dbc.com>
2013-07-05 15:21:04 -0700

    added take survey page with routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,15 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/take_survey' do
+  @survey = Survey.find(1)  # remove this stuff and put into take_survey post
+  erb :take_survey
+end
+
+post '/take_survey/:id' do 
+  @survey_name = Survey.find(params[:id])
+  @question_bank = @survey.questions
+  @answer_bank = @survey.questions.first.answers
+  erb :take_survey
+end
