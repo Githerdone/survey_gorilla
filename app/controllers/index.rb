a27313314cd52922b56a760c9732ad97285e5ee2
dbc-apprentice <dbc@dbc.com>
2013-07-05 15:42:03 -0700

    changes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,15 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+get '/dashboard/:id' do |id|
+  @user = User.find(id)
+  @surveys = @user.surveys
+  erb :dashboard
+end
+
+get '/create_survey/:id' do |id|
+  erb :create_survey
 end
 
 get '/take_survey' do
