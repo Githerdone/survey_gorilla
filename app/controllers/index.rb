93c9f1f52a816b0411ba58d47e7f8dc33014c576
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-05 15:11:51 -0700

    created dashboard

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,17 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+get '/dashboard/:id' do |id|
+  @user = User.find(id)
+  @surveys = @user.surveys
+  erb :dashboard
+end
+
+get '/create_survey/:id' do |id|
+  erb :create_survey
+end
+
+get '/take_survey/:id' do |id|
+  erb :take_survey
 end
